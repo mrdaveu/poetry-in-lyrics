b1ea291a0a3586f6cf2e2856f85d805ffe11f24e
mrdaveu <ulokwa@gmail.com>
2024-07-30 23:15:30 +0800

    reverse index order

--- .eleventy.js
+++ .eleventy.js
@@ -3,3 +3,10 @@ module.exports = function(eleventyConfig) {
    eleventyConfig.addPassthroughCopy("./fonts");
    eleventyConfig.addPassthroughCopy("./media");
 };
+
+exports.render = function (data) {
+	let posts = data.collections.post.reverse();
+	return `<ul>
+    ${posts.map((post) => `<li>${post.data.title}</li>`).join("\n")}
+  </ul>`;
+};
